90b3bd72071773cc7fe5d2914afeae96f32f5ef3
Jack O'Connor <jacko@fb.com>
2013-01-23 14:03:53 -0800

    Enable the inspector

--- messenger.py
+++ messenger.py
@@ -7,13 +7,19 @@ from PyQt4.QtCore import *
 from PyQt4.QtGui import *
 from PyQt4.QtWebKit import *
 
-app = QApplication(sys.argv)
+def main():
+  app = QApplication(sys.argv)
 
-web = QWebView()
-web.load(QUrl("https://www.facebook.com/desktop/client"))
-web.resize(200,600)
-web.show()
+  web = QWebView()
+  web.page().settings().setAttribute(
+      QWebSettings.DeveloperExtrasEnabled, True)
+  web.load(QUrl("https://www.facebook.com/desktop/client"))
+  web.resize(200,600)
+  web.show()
 
-# enable quitting with ctrl-c
-signal.signal(signal.SIGINT, signal.SIG_DFL)
-sys.exit(app.exec_())
+  # enable quitting with ctrl-c
+  signal.signal(signal.SIGINT, signal.SIG_DFL)
+  sys.exit(app.exec_())
+
+if __name__=="__main__":
+  main()
