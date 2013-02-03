db0c510e173f16ce5882281ca92cbf86fe84124b
Jack O'Connor <jacko@fb.com>
2013-02-02 16:26:22 -0800

    set API level 2 for PyQt, for Python 2/3 compat

--- messenger.py
+++ messenger.py
@@ -1,5 +1,12 @@
 #!/usr/bin/env python3
 
+# Python2 defaults to PyQt4's API level 1, but Python3
+# defaults to level 2, which is what we want. For
+# compatibility, we have to explicitly ask for level 2.
+import sip
+for module in ("QString", "QUrl"):
+  sip.setapi(module, 2)
+
 import sys
 import signal
 from PyQt4 import QtGui
