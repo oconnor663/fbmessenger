8c637f62c78b103bf31dd42b5ecdc18d90a0b0da
Jack O'Connor <oconnor663@gmail.com>
2013-05-17 11:14:16 -0700

    initial icon

--- setup.py
+++ setup.py
@@ -2,14 +2,19 @@
 
 from setuptools import setup
 
-setup(name="fbmessenger",
-      version="0.1.0",
-      description="Linux compatible Python reimplementation of "
-                  "Facebook Messenger for Windows",
-      author="Jack O'Connor",
-      author_email="oconnor663@gmail.com",
-      url="https://github.com/oconnor663/linuxmessenger",
-      packages=['fbmessengerlib'],
-      package_data={'fbmessengerlib': ['resources/*']},
-      data_files=[('share/applications', ['fbmessenger.desktop'])],
-      scripts=['fbmessenger'])
+setup(
+    name="fbmessenger",
+    version="0.1.0",
+    description="Linux compatible Python reimplementation of "
+    "Facebook Messenger for Windows",
+    author="Jack O'Connor",
+    author_email="oconnor663@gmail.com",
+    url="https://github.com/oconnor663/linuxmessenger",
+    packages=['fbmessengerlib'],
+    package_data={'fbmessengerlib': ['resources/*']},
+    data_files=[
+      ('share/applications', ['fbmessenger.desktop']),
+      ('share/fbmessenger', ['icon.png']),
+      ],
+    scripts=['fbmessenger'],
+    )
